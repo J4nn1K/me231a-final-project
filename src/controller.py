cdc0146a4d73d119a175fb974f50c7293b4793a3
JustusKa <justus.karnath@tuhh.de>
2022-12-12 22:02:38 -0800

    Add LQR

--- src/controller.py
+++ src/controller.py
@@ -1,5 +1,6 @@
 import pyomo.environ as pyo
 import numpy as np
+import scipy
 
 class MPC():
   def __init__(self):
@@ -15,6 +16,8 @@ class MPC():
     self.I1 = model_parameters.I1
     self.I2 = model_parameters.I2
     self.g = model_parameters.g
+    self.f1 = model_parameters.f1
+    self.f2 = model_parameters.f2
 
   def set_controller_params(self, controller_parameters):
     self.N = controller_parameters.N
@@ -48,6 +51,8 @@ class MPC():
     model.l2 = self.l2
     model.I1 = self.I1
     model.I2 = self.I2
+    model.f1 = self.f1
+    model.f2 = self.f2
 
     model.g = self.g
     model.last_t = None
@@ -138,17 +143,20 @@ class LinearReferenceMPC(MPC):
         
         model.Q = -np.array([[1.0*self.g*self.l1*self.m1 - self.g*self.m2*(-1.0*self.L1 - 1.0*self.l2), 1.0*self.g*self.l2*self.m2],
                       [1.0*self.g*self.l2*self.m2, 1.0*self.g*self.l2*self.m2]])
+        model.F = np.array([[self.f1,0],[0,self.f2]])
         model.B = np.array([[1],[0]])
         
         model.MQ = np.zeros_like(model.Q)
+        model.MF = np.zeros_like(model.F)
         model.MB = np.zeros_like(model.B)
         for i2 in range(2):
           for k in range(2):
             model.MQ[i2,k] = sum(model.M_inv[i2,j] * model.Q[j,k] for j in range(2))
+            model.MF[i2,k] = sum(model.M_inv[i2,j] * model.F[j,k] for j in range(2))
           model.MB[i2,0] = sum(model.M_inv[i2,j] * model.B[j,0] for j in range(2))
           coordinate_diff = np.array([np.pi,0])
       
-      return (model.x[i, t+1] == model.x[i, t] + self.dt*(sum(-model.MQ[i-2, j] * (model.x[j, t]-coordinate_diff[j]) for j in range(2))
+      return (model.x[i, t+1] == model.x[i, t] + self.dt*(sum(-model.MQ[i-2, j] * (model.x[j, t]-coordinate_diff[j]) - model.MF[i-2, j] * model.x[j+2, t] for j in range(2))
                                  +  model.MB[i-2, 0] * model.u[0, t//self.x_fac] )) if t < model.N*self.x_fac else pyo.Constraint.Skip
 
 class PID():
@@ -162,3 +170,47 @@ class PID():
     
     return u
 
+class LQR():
+  def __init__(self):
+    pass
+
+  def set_model_params(self, model_parameters):
+    self.m1 = model_parameters.m1
+    self.m2 = model_parameters.m2
+    self.L1 = model_parameters.L1
+    self.L2 = model_parameters.L2
+    self.l1 = model_parameters.l1
+    self.l2 = model_parameters.l2
+    self.I1 = model_parameters.I1
+    self.I2 = model_parameters.I2
+    self.g = model_parameters.g
+    self.f1 = model_parameters.f1
+    self.f2 = model_parameters.f2
+    
+  def set_controller_params(self, Q, R):
+    self.Q = Q
+    self.R = R
+
+  def get_K(self):
+    M_inv = np.linalg.inv(np.array([[self.I1 + self.I2 + self.L1**2*self.m2 + 2*self.L1*self.l2*self.m2, self.I2 + self.L1*self.l2*self.m2],
+                                    [self.I2 + self.L1*self.l2*self.m2, self.I2]]))
+    Q = -np.array([[1.0*self.g*self.l1*self.m1 - self.g*self.m2*(-1.0*self.L1 - 1.0*self.l2), 1.0*self.g*self.l2*self.m2],
+                      [1.0*self.g*self.l2*self.m2, 1.0*self.g*self.l2*self.m2]])
+    F = np.array([[self.f1,0],[0,self.f2]])
+            
+    A = np.block([[np.zeros((2,2)), np.eye(2)],
+                  [-M_inv @ Q, -M_inv @ F]])#.reshape(4,4)
+    #print(A)
+    B = np.array([[1],[0]])
+
+    B = np.block([[np.zeros((2,1))],
+                  [-M_inv@B]])
+
+    # solve Discrete Algebraic Riccatti equation  
+    P = scipy.linalg.solve_continuous_are(A, B, self.Q, self.R)
+
+    # compute the LQR gain
+    #K = scipy.linalg.inv(B.T @ P @ B + self.R) @ (B.T @ P @ A)
+    K = (B.T @ P)
+    return K
+  
--- src/simulator.py
+++ src/simulator.py
@@ -16,6 +16,8 @@ class Simulator():
     self.l2 = model_parameters.l2
     self.I1 = model_parameters.I1
     self.I2 = model_parameters.I2
+    self.f1 = model_parameters.f1
+    self.f2 = model_parameters.f2
 		
     self.g = model_parameters.g
 
@@ -24,7 +26,7 @@ class Simulator():
 
     t_eval = np.arange(0, self.dt, self.dt/10)
     
-    solution = solve_ivp(self._func, [0, self.dt], x, method='RK45', t_eval=t_eval)
+    solution = solve_ivp(self._func, [0, self.dt], x, method='BDF', t_eval=t_eval)
 
     return solution.y[:, -1]
 
@@ -38,12 +40,14 @@ class Simulator():
     tau = np.array([[-self.m1*self.g*self.l1*np.sin(x[0]) - self.m2*self.g*(self.L1*np.sin(x[0])+self.l2*np.sin(x[0]+x[1]))],
                     [-self.m2*self.g*self.l2*np.sin(x[0]+x[1])]])
 
+    F = np.array([[self.f1,0],[0,self.f2]])
+
     B = np.array([[1], [0]])
 
     f = np.zeros_like(x)
     f[0] = x[2]
     f[1] = x[3]
-    f[2:4] = (M_inv @ (-C @ x[2:4].reshape(2,) +
+    f[2:4] = (M_inv @ (-(C+F) @ x[2:4].reshape(2,) +
               tau.reshape(2,) + B.reshape(2,) * self.u)).T
         
     return f
